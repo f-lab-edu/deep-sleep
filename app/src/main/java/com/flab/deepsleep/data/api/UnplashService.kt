f16876dcdb89bd484bb52572d75a6280a5b363e8
dolchvita <yesoe0502@naver.com>
2024-12-27 17:13:34 +0900

    delete RandomPhoto

--- app/src/main/java/com/flab/deepsleep/data/api/UnplashService.kt
+++ app/src/main/java/com/flab/deepsleep/data/api/UnplashService.kt
@@ -1,6 +1,5 @@
 package com.flab.deepsleep.data.api
 
-import com.flab.deepsleep.data.entity.photos.RandomPhoto
 import com.flab.deepsleep.data.entity.photos.SinglePhoto
 import com.flab.deepsleep.data.entity.search.SearchPhotos
 import retrofit2.http.GET
@@ -13,7 +12,7 @@ interface UnplashService {
     suspend fun getRandomPhotos(
         @Query("client_id") clientId: String,
         @Query("count") count: Int = 1
-    ): List<RandomPhoto>
+    ): List<SinglePhoto>
 
     @GET("/search/photos")
     suspend fun getSearchPhotos(
--- app/src/main/java/com/flab/deepsleep/data/repo/UnplashRepository.kt
+++ app/src/main/java/com/flab/deepsleep/data/repo/UnplashRepository.kt
@@ -1,11 +1,10 @@
 package com.flab.deepsleep.data.repo
 
-import com.flab.deepsleep.data.entity.photos.RandomPhoto
 import com.flab.deepsleep.data.entity.photos.SinglePhoto
 import com.flab.deepsleep.data.entity.search.SearchPhotos
 
 interface UnplashRepository {
-    suspend fun getRandomPhotos(count: Int) : List<RandomPhoto>
+    suspend fun getRandomPhotos(count: Int) : List<SinglePhoto>
     suspend fun getSearchPhotos(query: String) : SearchPhotos
     suspend fun getAPhotoById(photoId: String) : SinglePhoto
 }
